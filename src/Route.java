2b9ab7177e54c712660beb16a0f694b70b4dafad
Nick Simons <>
2018-05-07 12:16:24 +0200

    Some classes

---
+++ src/Route.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Route {
+
+    private ArrayList<Object> route = new ArrayList<>();
+
+    public Route(String ad1, String ad2) {
+
+    }
+
+    public Route(String route) {
+        
+    }
+
+
+}
---
+++ src/Verzending.java
@@ -0,0 +1,2 @@
+public class Verzending {
+}
