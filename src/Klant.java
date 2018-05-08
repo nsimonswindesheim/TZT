e3c974b8c69e25b32e46551fea3e043a55c04efe
Dnew97 <38904742+Dnew97@users.noreply.github.com>
2018-05-08 14:49:53 +0200

    test
    push push

--- src/Klant.java
+++ src/Klant.java
@@ -1,2 +1,50 @@
 public class Klant {
+
+    private int klantID;
+    private String voornaam;
+    private String achternaam;
+    private String email;
+    private String telefoonnummer;
+    private String adres;
+    private String rekeningnummer;
+
+    public Klant(String voornaam, String achternaam, String email, String telefoonnummer, String adres, String rekeningnummer){
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.email = email;
+        this.telefoonnummer = telefoonnummer;
+        this.adres = adres;
+        this.rekeningnummer = rekeningnummer;
+    }
+
+    public Klant(int klantID) {
+    }
+
+    public String zoekBestaandeKlant(int klantID) {
+
+    }
+
+    public getVoornaam() {
+        return voornaam;
+    }
+
+    public getAchternaam() {
+        return achternaam;
+    }
+
+    public getEmail() {
+        return email;
+    }
+
+    public getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public getAdres() {
+        return adres;
+    }
+
+    public getRekeningnummer() {
+        return rekeningnummer;
+    }
 }
--- src/Verzending.java
+++ src/Verzending.java
@@ -1,2 +1,39 @@
 public class Verzending {
+
+    private int verzendID;
+    private Klant klant;
+    private Route route;
+    private String afhaalAdres;
+    private String bezorgAdres;
+
+    public Verzending(Klant klant, Route route, String afhaalAdres, String bezorgAdres) {
+        this.klant = klant;
+        this.route = route;
+        this.afhaalAdres = afhaalAdres;
+        this.bezorgAdres = bezorgAdres;
+    }
+
+    public Verzending(int verzendID) {
+
+    }
+
+    public String zoekBestaandeVerzending(int verzendID) {
+
+    }
+
+    public Klant getKlant() {
+        return klant;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public String getAfhaalAdres() {
+        return afhaalAdres;
+    }
+
+    public String getBezorgAdres() {
+        return bezorgAdres;
+    }
 }
