e5e5b5064ebffafca3b6375ea45c21252b9748fa
Nick Simons <>
2018-05-08 15:04:51 +0200

    Database setup
    Database setup + database connection code

---
+++ src/DBConnection.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String DRIVER      = "org.apache.derby.jdbc.EmbeddedDriver";
+    private static final String JDBC_URL    = "jdbc:derby:DBTZT;create=true";
+
+    public Connection conn;
+
+    public DBConnection() {
+        try {
+            this.conn = DriverManager.getConnection(JDBC_URL);
+            if(this.conn != null) {
+                System.out.println("Connected to database");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void query(String q) {
+        try {
+            this.conn.createStatement().execute("");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
